9fbf0d93e335c7432ec2ca92d6d5fe75196c62be
Jpeterson <peterson.jacob13@protonmail.com>
2025-04-19 20:23:31 -0500

    added api routes

--- routes/api/thoughtRoutes.ts
+++ routes/api/thoughtRoutes.ts
@@ -2,4 +2,15 @@ import { Router } from 'express';
 
 const router = Router();
 
+import thoughtIdRoutes from './thoughtIdRoutes.js'
 
+import { getThoughts, createThought, } from '../../controlers/thoughtController.js';
+
+router.use('/:thoughtId', thoughtIdRoutes)
+
+router
+    .route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+export default router
